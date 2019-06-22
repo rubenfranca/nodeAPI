03d108e333f4d4efb84d82c41362a8863bed8678
rubenfranca <ruben_tiagof@hotmail.com>
2019-06-22 22:16:17 +0100

    Add password encryption

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,5 +24,35 @@ const userSchema = new mongoose.Schema({
     },
     updated: Date
 });
+
+// virtual field
+userSchema.virtual('password')
+    .set(function(password) {
+        // create temporary variable called _password
+        this.password = password;
+
+        // generate a timestamp
+        this.salt = uuidv1();
+
+        // encrypt the password
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+// methods
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            return '';
+        }
+    }
+}
 
 module.exports = mongoose.model('User', userSchema);
