3732edb21a795c0bfbaf31fa5a2e84fa1c3b0bd7
rubenfranca <ruben_tiagof@hotmail.com>
2019-06-22 15:16:54 +0100

    Add routes and controllers

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', (req, res) => res.send('Hello world from node JS'));
+// Bring in the routes
+const postRoutes = require('./routes/post');
+
+// middleware
+app.use(morgan('dev'));
+
+app.use('/', postRoutes);
 
 const port = 8080;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
