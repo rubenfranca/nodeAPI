20f89825b7ac011c056001be205a5b89817040f8
rubenfranca <ruben_tiagof@hotmail.com>
2019-06-22 15:40:37 +0100

    Add mongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => console.log(`DB Connection Error: ${err}`));
 
 // Bring in the routes
 const postRoutes = require('./routes/post');
@@ -10,5 +19,5 @@ app.use(morgan('dev'));
 
 app.use('/', postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
